f3c94f41fad769793fa2ce8f5eedbe251af0b7d7
mfoeken <martijn.van.foeken@gmail.com>
2022-04-12 14:35:58 +0200

    Update echart_main.js

--- echart_main.js
+++ echart_main.js
@@ -31,21 +31,68 @@ var getScriptPromisify = (src) => {
     }
 
 
-
-set myDataSource(dataBinding){this._myDataSource=dataBindingthis.render()}
-
+      set myDataSource (dataBinding) {
+      this._myDataSource = dataBinding
+      this.render()
+    }
 
     async render () {
       await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
 
-
-const option={tooltip:{trigger:'item'},legend:{top:'2%',left:'center'},series:[{name:'',type:'pie',radius:['40%','65%'],avoidLabelOverlap:false,itemStyle:{borderRadius:10,borderColor:'#fff',borderWidth:4},label:{show:false,position:'center'},emphasis:{label:{show:true,fontSize:'25',fontWeight:'bold'}},labelLine:{show:false},data}]}myChart.setOption(option)}}
-
+      if (!this._myDataSource || this._myDataSource.state !== 'success') {
+        return
+      }
+   
+      const dimension = this._myDataSource.metadata.feeds.dimensions.values[0]
+      const measure = this._myDataSource.metadata.feeds.measures.values[0]
+      const data = this._myDataSource.data.map(data => {
+        return {
+          name: data[dimension].label,
+          value: data[measure].raw
+        }
+      })
 
       const myChart = echarts.init(this._root, 'wight')
+      const option = {
+        tooltip: {
+          trigger: 'item'
+        },
+        legend: {
+          top: '2%',
+          left: 'center'
+        },
+        series: [
+          {
+            name: '',
+            type: 'pie',
+            radius: ['40%', '65%'],
+            avoidLabelOverlap: false,
+            itemStyle: {
+              borderRadius: 10,
+              borderColor: '#fff',
+              borderWidth: 4
+            },
+            label: {
+              show: false,
+              position: 'center'
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: '25',
+                fontWeight: 'bold'
+              }
+            },
+            labelLine: {
+              show: false
+            },
+            data
+          }
+        ]
+      }
+      myChart.setOption(option)
+    }
+  }
 
-const option={tooltip:{trigger:'item'},legend:{top:'2%',left:'center'},series:[{name:'',type:'pie',radius:['40%','65%'],avoidLabelOverlap:false,itemStyle:{borderRadius:10,borderColor:'#fff',borderWidth:4},label:{show:false,position:'center'},emphasis:{label:{show:true,fontSize:'25',fontWeight:'bold'}},labelLine:{show:false},data}]}myChart.setOption(option)}}
-
-
-  customElements.define('com-sap-sample-echarts-pie_chart_demo_xx', SamplePieChart)
+  customElements.define('com-sap-sample-echarts-pie_chart', SamplePieChart)
 })()
